123a523590c0281010378cc500ac97823bd98416
Kousuke Kikuchi <kousukekikuchi@toki.waseda.jp>
2014-11-03 10:04:42 +0900

    completed 1.1 and 1.2

--- preparation/1110.scm
+++ preparation/1110.scm
@@ -43,3 +43,61 @@
 
 (define (>= x y)
     (not (< x y)))
+
+
+;; Excercise 1.1
+10
+;; 10
+(+ 5 3 4)
+;; 12
+(- 9 1)
+;; 8
+(/ 6 2)
+;; 3
+(+ (* 2 4) (- 4 6))
+;; 6
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+;; 19
+(= a b)
+;; #f
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+;; 4 , returns b
+(cond ((= a 4) 6)
+       (= b 4) (+ 6 7 a)
+       (else 25))
+;; 4
+(+ 2 (if (> b a) b a))
+;; 6
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
+;; 16
+
+;; Excercise 1.2
+(/ (+ 5 4
+      (- 2
+         (- 3
+            (+ 6
+               (/ 4 5)))))
+   (* 3
+      (- 6 2)
+      (- 2 7)))
+
+
+;; Excercise 1.3    動かない。。。
+(define (test d e)
+  (+ (* d d) (* e e)))
+(define (func a b c)
+  (cond ((and (> a b) (> b c))
+         (test a b))
+        ((and (> b c) (> c a))
+         (test b c))
+        ((and (> c a) (> a b))
+         (test c a))))
+(func 1 2 3)
+
