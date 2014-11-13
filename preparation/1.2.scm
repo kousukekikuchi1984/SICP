2608d4ea024c67ad63258956efbdf8e7669e2980
Kousuke Kikuchi <kousukekikuchi@toki.waseda.jp>
2014-11-13 21:14:02 +0900

    colve 1.11 1.12

--- preparation/1.2.scm
+++ preparation/1.2.scm
@@ -107,11 +107,88 @@
 (define (h n) (A 2 n))  ; hn = (n+1) * 2^(n+1) ?
 
 ;1.2.2
+;木構造再帰プロセス
 (define (fib n)
   (cond ((= n 0) 0)
         ((= n 1) 1)
         (else (+ (fib (- n 1))
                  (fib (- n 2))))))
+; この方法だと、計算量は木構造の節の数に比例するので、n^2
+
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+    b
+    (fib-iter (+ a b) a (- count 1))))
+
+;; countで何回繰り返したかを計算 aとbで値を保存
+;; 計算量はn倍になる
+;; 木構造は問題を考える上でシンプルになる。ただ、それを普通に適応すると、計算量が必要になる
+;; しかし、計算量を抑える効率的な設計にすれば良い。
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kind-of-coins)
+  cond ((= amount 0) 1)
+       ((or (< amount 0) (= kind-of-coins 0)) 0)
+       (else (+ (cc amount (- kind-of-coins 1))
+                (cc (- amount (first-denomination kind-of-coins))
+                    kind-of-coins))))
+
+(define (first-denomination kind-of-coins)
+  (cond ((= kind-of-coins 1) 1)
+        ((= kind-of-coins 2) 5)
+        ((= kind-of-coins 3) 10)
+        ((= kind-of-coins 4) 25)
+        ((= kind-of-coins 5) 50)))
+
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;; 1.11
+(define (func n)
+  (cond (>= n 3)
+        (+ (func (- n 1) (* 2 (func (- n 2)) (* 3 (func ((- n 3)))))))))
+
+(define (func n)
+  (func-iter 1 1 1 n 0))
+
+(define (func-iter n-1 n-2 n-3 n state)
+  (if (= state n)
+    n-1
+    (func-iter (+ n-1 (* 2 n-2) (* 3 n-3)) n-1 n-2 n (+ state 1))))
+;;
+
+;1.12
+
+(define (pascal n x)
+  (if (or (< n 3) (= x 1) (= x n))
+    1
+    (+ (pascal (- n 1) (+ x 1)) (pascal (- n 1) x))))
 
 
 
+;; 1.13 pass
+;;
+;;
+;;
