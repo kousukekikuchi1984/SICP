28bc74ce0f494d307cc6db44ec6daac1a3fbabc7
kousukekikuchi <kousuke_kikuchi@spotlig.ht>
2014-11-13 12:03:04 +0900

    add

--- preparation/1.2.scm
+++ preparation/1.2.scm
@@ -106,5 +106,12 @@
 (define (g n) (A 1 n))  ; gn = 2^n
 (define (h n) (A 2 n))  ; hn = (n+1) * 2^(n+1) ?
 
+;1.2.2
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
 
 
