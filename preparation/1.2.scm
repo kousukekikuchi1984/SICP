10f935ac6f14d9b0c6c018b200145b1951dfaf37
Kousuke Kikuchi <kousukekikuchi@toki.waseda.jp>
2014-11-22 06:05:39 +0900

    add something

--- preparation/1.2.scm
+++ preparation/1.2.scm
@@ -445,3 +445,22 @@
   y)
 (test 0 (p))
 
+
+; 1.2.6
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+
+(define (divides? )a i
+  (= (remainder b a) 0))
+
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
